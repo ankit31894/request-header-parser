01582fb601b7ade7cdab88781f6c089bc1abb6fb
Ankit <ankit31894@gmail.com>
2016-06-02 01:20:28 +0530

    Initial files

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var express = require("express");
 var http = require("http");
+var useragent=require("useragent");
 var app = express();
 
 app.all("*", function(req, response, next) {
+    var agent = useragent.parse(req.headers['user-agent']);
   response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("D"+JSON.stringify(req.headers['x-forwarded-for'] || 
-     req.connection.remoteAddress || 
-     req.socket.remoteAddress ||
-     req.connection.socket.remoteAddress));
+    response.end(JSON.stringify({
+        "IP address":req.headers['x-forwarded-for'] || 
+                        req.connection.remoteAddress || 
+                        req.socket.remoteAddress ||
+                        req.connection.socket.remoteAddress,
+        "language":req.headers["accept-language"],
+        "Operating System":agent.os.toString()}));
     next();
 });
 http.createServer(app);
